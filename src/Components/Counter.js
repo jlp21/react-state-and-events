21b94a03354d51cf5d6b0d16769db4d60d29ee0a
jlp21 <portelli21@gmail.com>
2022-03-01 21:19:52 -0500

    adding counter

---
+++ src/Components/Counter.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+ 
+function Counter() {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+   
+    return (
+      <div className="Counter">
+        <h2>Counter</h2>
+   
+        <p> You clicked { count } times! </p> {/* <-- use the "count" variable here */}
+   
+        <button> - </button>
+        <button> + </button>
+      </div>
+    );
+  }
+ 
+export default Counter;
