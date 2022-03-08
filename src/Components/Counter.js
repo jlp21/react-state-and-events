3de6c07efb045a63018db382ddc40b36e2713db1
jlp21 <portelli21@gmail.com>
2022-03-07 21:42:32 -0500

    updating code

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
 
 function Counter() {
-    // Declare a new state variable, which we'll call "count"
-    const [count, setCount] = useState(() => 0);
-
+    const [count, setCount] = useState(0);
+   
     return (
-        <div className="Counter">
-            <h2>Counter</h2>
-
-            <p> You clicked {count} times! </p> {/* <-- use the "count" variable here */}
-
-            <button onClick={() => setCount(count - 1)}> - </button>
-            <button onClick={() => setCount(count + 1)}> + </button>
-        </div>
+      <div className="Counter">
+        <h2>Counter</h2>
+   
+        <p>You clicked {count} times</p>
+   
+        <button onClick={() => setCount((prevCount) => prevCount - 1)}> - </button>
+        <button onClick={() => setCount((prevCount) => prevCount + 1)}> + </button>
+      </div>
     );
-}
-
-export default Counter;
+  }
+   
+  export default Counter;
