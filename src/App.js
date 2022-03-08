90b3334b352f3ad1268b128c8749994b38652578
jlp21 <portelli21@gmail.com>
2022-03-07 21:47:51 -0500

    added dark & light theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Counter from './Components/Counter';
- 
+import { useState } from 'react';
+
 function App() {
+
+  const [theme, setTheme] = useState('light');
+
   return (
-    <div className="App ">
+    <div className={'App ' + theme}>
       <h1>React - state and events</h1>
- 
+
       <Counter />
+
+      <select onChange={event => setTheme(event.target.value)}>
+        <option value="light"> Light </option>
+        <option value="dark"> Dark </option>
+      </select>
     </div>
   );
 }
