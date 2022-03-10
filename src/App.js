0f7f2cd6e76b8e3870fbd3204567917da8131623
jlp21 <portelli21@gmail.com>
2022-03-09 21:55:05 -0500

    refactoring

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import Counter from './Components/Counter';
 import { useState } from 'react';
 
 function App() {
-
   const [theme, setTheme] = useState('light');
-
+ 
+  const toggleTheme = event => {
+    setTheme(event.target.value);
+  };
+ 
   return (
     <div className={'App ' + theme}>
       <h1>React - state and events</h1>
-
       <Counter />
-
-      <select onChange={event => setTheme(event.target.value)}>
+ 
+      <select onChange={ toggleTheme }>
         <option value="light"> Light </option>
         <option value="dark"> Dark </option>
       </select>
     </div>
   );
 }
-
+ 
 export default App;
